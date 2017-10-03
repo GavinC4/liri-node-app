c5e43723bf13b0ea87e4cc1b707dbf2746433cd1
Gavin Clay <cgavin40@yahoo.com>
2017-10-03 11:56:14 -0400

    cant find the bug at the moment

--- keys.js
+++ keys.js
@@ -1,10 +1,19 @@
 console.log('this is loaded');
 
 var twitterKeys = {
-  consumer_key: '<input here>',
-  consumer_secret: '<input here>',
-  access_token_key: '<input here>',
-  access_token_secret: '<input here>',
+  consumer_key: '9LI4OGNy2h8QekiEsAsFqHh77',
+  consumer_secret: 'lXZ76No85bwhTwIlKegLsg8moIZre08ilbaUirDV5REY3q0Yrv',
+  access_token_key: '915235313336889344-tILZiv8GSHpKlLXMCMlb0a01tTU9F9H',
+  access_token_secret: 'm7f7EFttOTSRgMguijlvb8axAODTS1Bp9F74dV0F1kaM6',
 }
 
 module.exports = twitterKeys;
+
+var spotifyKeys = {
+  consumer_key: '09a7f6c043104324bc035b7f1ff797aa',
+  consumer_secret: '5772b051824b4d168bfb64e9a986347d',
+  // access_token_key: '',
+  // access_token_secret: '',
+}
+
+module.exports = spotifyKeys;
